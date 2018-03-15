0039186ed4d28791a0e39f167a98b27d26dc9540
Alberto Trevizo <betot.saenz@gmail.com>
2018-03-15 16:06:24 -0700

    Se agrega modelo de Productos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Producto.create(
+    [{:nombre => 'Papitas', :precio => 12},
+     {:nombre => 'Dona', :precio => 10},
+     {:nombre => 'Soda', :precio => 8}
+    ])
